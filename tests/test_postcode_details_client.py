5be39d56eaa9ed69271f87a6704e37184be80927
Conor McMahon <ConorRisnerMcMahon@gmail.com>
2021-11-14 22:57:27 +0200

    Refactor tests to use fixtures for search types
    Reduce duplication and reduce number of integration tests by only
    testing the fixtures against the real api rather than duplicating
    each test

--- tests/test_postcode_details_client.py
+++ tests/test_postcode_details_client.py
@@ -1,124 +1,117 @@
-from app.postcode_details_client import PostcodeDetailsClient
+from app.postcode_details_client import PostcodeDetailsClient, POSTCODES_IO_URL
 from hamcrest import assert_that, equal_to, is_
 from unittest.mock import patch
 import pytest
+import requests
 
 
-@patch('app.postcode_details_client.requests.get')
-def test_can_get_LSOA_for_postcode(mock_get):
-    mock_get.return_value.ok = True
-    mock_get.return_value.json.return_value = {
-        "status": 200,
-        "result": {
-            "postcode": "SE5 0NF",
-            "lsoa": "Southwark 017A"
+@pytest.fixture
+def valid_postcode_with_search_result():
+    with patch('app.postcode_details_client.requests.get') as mock_get:
+        mock_get.return_value.ok = True
+        mock_get.return_value.json.return_value = {
+            "status": 200,
+            "result": {
+                "postcode": "SE5 0NF",
+                "lsoa": "Southwark 017A"
+            }
         }
-    }
-    postcode_details_client = PostcodeDetailsClient(postcode='SE5 0NF')
-    assert_that(postcode_details_client.lsoa, equal_to('Southwark 017A'))
+        return PostcodeDetailsClient(postcode='SE5 0NF')
 
 
 @pytest.mark.webtest
-def test_can_get_LSOA_for_postcode_webtest():
-    postcode_details_client = PostcodeDetailsClient(postcode='SE5 0NF')
-    assert_that(postcode_details_client.lsoa, equal_to('Southwark 017A'))
+def test_valid_postcode_with_search_result_fixture_integration():
+    resp = requests.get(f'{POSTCODES_IO_URL}/SE5 0NF')
+    resp_json = resp.json()
+    assert_that(resp_json['status'], equal_to(200))
+    assert_that(resp_json['result']['postcode'], equal_to('SE5 0NF'))
+    assert_that(isinstance(resp_json['result']['lsoa'], str), is_(True))
 
 
-@patch('app.postcode_details_client.requests.get')
-def test_can_tell_if_a_search_result_was_returned(mock_get):
-    mock_get.return_value.ok = False
-    mock_get.return_value.json.return_value = {
-        "status": 404,
-        "error": "Invalid postcode"
-    }
-    postcode_details_client = PostcodeDetailsClient(postcode='SE5 NF')
-    assert_that(postcode_details_client.result_found, is_(False))
-    mock_get.return_value.ok = True
-    mock_get.return_value.json.return_value = {
-        "status": 200,
-        "result": {
-            "postcode": "SE5 0NF",
-            "lsoa": "Southwark 017A"
+@pytest.fixture
+def valid_postcode_no_search_result():
+    with patch('app.postcode_details_client.requests.get') as mock_get:
+        mock_get.return_value.ok = False
+        mock_get.return_value.json.return_value = {
+            "status": 404,
+            "error": "Postcode not found"
         }
-    }
-    postcode_details_client = PostcodeDetailsClient(postcode='SE5 0NF')
-    assert_that(postcode_details_client.result_found, is_(True))
+        return PostcodeDetailsClient(postcode='SH24 1AA')
 
 
 @pytest.mark.webtest
-def test_can_tell_if_a_search_result_was_returned_webtest():
-    postcode_details_client = PostcodeDetailsClient(postcode='SE5 NF')
-    assert_that(postcode_details_client.result_found, is_(False))
-    postcode_details_client = PostcodeDetailsClient(postcode='SE5 0NF')
-    assert_that(postcode_details_client.result_found, is_(True))
+def test_valid_postcode_no_search_result_fixture_integration():
+    resp = requests.get(f'{POSTCODES_IO_URL}/SH24 1AA')
+    resp_json = resp.json()
+    assert_that(resp_json['status'], equal_to(404))
+    assert_that(resp_json['error'], equal_to('Postcode not found'))
 
 
-@patch('app.postcode_details_client.requests.get')
-def test_can_tell_if_a_postcode_is_invalid(mock_get):
-    mock_get.return_value.ok = False
-    mock_get.return_value.json.return_value = {
-        "status": 404,
-        "error": "Invalid postcode"
-    }
-    postcode_details_client = PostcodeDetailsClient(postcode='SE13BlahBlah')
-
-    assert_that(postcode_details_client.invalid_postcode, is_(True))
-    mock_get.return_value.ok = True
-    mock_get.return_value.json.return_value = {
-        "status": 200
-    }
-    postcode_details_client = PostcodeDetailsClient(postcode='SE5 0NF')
-
-    assert_that(postcode_details_client.invalid_postcode, is_(False))
-    mock_get.return_value.ok = False
-    mock_get.return_value.json.return_value = {
-        "status": 200,
-        "error": "Postcode not found"
-    }
-    postcode_details_client = PostcodeDetailsClient(postcode='SH24 1AA')
-
-    assert_that(postcode_details_client.invalid_postcode, is_(False))
+@pytest.fixture
+def invalid_postcode():
+    with patch('app.postcode_details_client.requests.get') as mock_get:
+        mock_get.return_value.ok = False
+        mock_get.return_value.json.return_value = {
+            "status": 404,
+            "error": "Invalid postcode"
+        }
+        return PostcodeDetailsClient(postcode='SE13BlahBlah')
 
 
 @pytest.mark.webtest
-def test_can_tell_if_a_postcode_is_invalid_webtest():
-    postcode_details_client = PostcodeDetailsClient(postcode='SE13BlahBlah')
-    assert_that(postcode_details_client.invalid_postcode, is_(True))
-    postcode_details_client = PostcodeDetailsClient(postcode='SE5 0NF')
-    assert_that(postcode_details_client.invalid_postcode, is_(False))
-    postcode_details_client = PostcodeDetailsClient(postcode='SH24 1AA')
-    assert_that(postcode_details_client.invalid_postcode, is_(False))
+def test_invalid_postcode_fixture_integration():
+    resp = requests.get(f'{POSTCODES_IO_URL}/SE13BlahBlah')
+    resp_json = resp.json()
+    assert_that(resp_json['status'], equal_to(404))
+    assert_that(resp_json['error'], equal_to('Invalid postcode'))
 
 
-@patch('app.postcode_details_client.requests.get')
-def test_search_failed_to_return_result_for_unknown_reason(mock_get):
-    mock_get.return_value.ok = False
-    mock_get.return_value.json.return_value = {
-        "status": 404,
-        "error": "Invalid postcode"
-    }
-    postcode_details_client = PostcodeDetailsClient(postcode='SE13BlahBlah')
+@pytest.fixture
+def failed_search():
+    with patch('app.postcode_details_client.requests.get') as mock_get:
+        mock_get.return_value.ok = False
+        mock_get.return_value.json.return_value = {
+            "status": 500,
+            "error": "Some other error"
+        }
+        return PostcodeDetailsClient(postcode='SE5 0NF')
 
-    assert_that(postcode_details_client.search_successful, is_(True))
-    mock_get.return_value.ok = True
-    mock_get.return_value.json.return_value = {
-        "status": 200
-    }
-    postcode_details_client = PostcodeDetailsClient(postcode='SE5 0NF')
 
-    assert_that(postcode_details_client.search_successful, is_(True))
-    postcode_details_client = PostcodeDetailsClient(postcode='SH24 1AA')
-    mock_get.return_value.ok = False
-    mock_get.return_value.json.return_value = {
-        "status": 200,
-        "error": "Postcode not found"
-    }
-    assert_that(postcode_details_client.search_successful, is_(True))
-    mock_get.return_value.ok = False
-    mock_get.return_value.json.return_value = {
-        "status": 500,
-        "error": "Some other error"
-    }
-    postcode_details_client = PostcodeDetailsClient(postcode='SH24 1AA')
+def test_can_get_LSOA_for_postcode(valid_postcode_with_search_result):
+    assert_that(valid_postcode_with_search_result.lsoa, equal_to('Southwark 017A'))
 
-    assert_that(postcode_details_client.search_successful, is_(False))
+
+def test_can_tell_if_search_was_successful(
+    valid_postcode_with_search_result,
+    valid_postcode_no_search_result,
+    invalid_postcode,
+    failed_search
+):
+    assert_that(valid_postcode_with_search_result.search_successful, is_(True))
+    assert_that(valid_postcode_no_search_result.search_successful, is_(True))
+    assert_that(invalid_postcode.search_successful, is_(True))
+    assert_that(failed_search.search_successful, is_(False))
+
+
+def test_can_tell_if_search_found_a_result(
+    valid_postcode_with_search_result,
+    valid_postcode_no_search_result,
+    invalid_postcode,
+    failed_search
+):
+    assert_that(valid_postcode_with_search_result.result_found, is_(True))
+    assert_that(valid_postcode_no_search_result.result_found, is_(False))
+    assert_that(invalid_postcode.result_found, is_(False))
+    assert_that(failed_search.result_found, is_(False))
+
+
+def test_can_tell_if_postcode_is_invalid(
+    valid_postcode_with_search_result,
+    valid_postcode_no_search_result,
+    invalid_postcode,
+    failed_search
+):
+    assert_that(valid_postcode_with_search_result.invalid_postcode, is_(False))
+    assert_that(valid_postcode_no_search_result.invalid_postcode, is_(False))
+    assert_that(invalid_postcode.invalid_postcode, is_(True))
+    assert_that(failed_search.invalid_postcode, is_(False))
