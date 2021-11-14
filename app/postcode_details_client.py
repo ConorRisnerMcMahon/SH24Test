17544ccd8663af9c7545a06a2acd1a54992b005a
Conor McMahon <ConorRisnerMcMahon@gmail.com>
2021-11-14 20:39:21 +0200

    Check if PostcodeDetailsClient search succeeded
    Adds methods to PostcodeDetailsClient to distinguish between the
    cases where:
    - The search returned a result
    - The search did not return a result, and the supplied postcode
    is a syntactically valid UK postcode
    - The search did not return a result, and the supplied postcode
    is not a syntactically valid UK postcode
    - The search failed to return a result for some other unknown
    reason

--- app/postcode_details_client.py
+++ app/postcode_details_client.py
@@ -21,10 +21,24 @@ class PostcodeDetailsClient:
         return self.response
 
     @property
+    def status(self) -> int:
+        return self.search_result['status']
+
+    @property
+    def search_successful(self) -> bool:
+        return self.result_found or self.result_not_found or self.invalid_postcode
+
+    @property
     def result_found(self) -> bool:
-        if self.search_result['status'] == 200:
-            return True
-        return False
+        return self.status == 200
+
+    @property
+    def result_not_found(self) -> bool:
+        return self.status == 404 and self.search_result['error'] == 'Postcode not found'
+
+    @property
+    def invalid_postcode(self) -> bool:
+        return self.status == 404 and self.search_result['error'] == 'Invalid postcode'
 
     @property
     def lsoa(self) -> Optional['str']:
--- tests/test_postcode_details_client.py
+++ tests/test_postcode_details_client.py
@@ -51,9 +51,86 @@ def test_can_tell_if_a_search_result_was_returned(mock_get):
         "error": "Invalid postcode"
     }
     assert_that(postcode_details_client.result_found, is_(False))
+    postcode_details_client = PostcodeDetailsClient(postcode='SE5 0NF')
+    mock_get.return_value.ok = True
+    mock_get.return_value.json.return_value = {
+        "status": 200,
+        "result": {
+            "postcode": "SE5 0NF",
+            "lsoa": "Southwark 017A"
+        }
+    }
+    assert_that(postcode_details_client.result_found, is_(True))
 
 
 @pytest.mark.webtest
 def test_can_tell_if_a_search_result_was_returned_webtest():
     postcode_details_client = PostcodeDetailsClient(postcode='SE5 NF')
     assert_that(postcode_details_client.result_found, is_(False))
+    postcode_details_client = PostcodeDetailsClient(postcode='SE5 0NF')
+    assert_that(postcode_details_client.result_found, is_(True))
+
+
+@patch('app.postcode_details_client.requests.get')
+def test_can_tell_if_a_postcode_is_invalid(mock_get):
+    postcode_details_client = PostcodeDetailsClient(postcode='SE13BlahBlah')
+    mock_get.return_value.ok = False
+    mock_get.return_value.json.return_value = {
+        "status": 404,
+        "error": "Invalid postcode"
+    }
+    assert_that(postcode_details_client.invalid_postcode, is_(True))
+    postcode_details_client = PostcodeDetailsClient(postcode='SE5 0NF')
+    mock_get.return_value.ok = True
+    mock_get.return_value.json.return_value = {
+        "status": 200
+    }
+    assert_that(postcode_details_client.invalid_postcode, is_(False))
+    postcode_details_client = PostcodeDetailsClient(postcode='SH24 1AA')
+    mock_get.return_value.ok = False
+    mock_get.return_value.json.return_value = {
+        "status": 200,
+        "error": "Postcode not found"
+    }
+    assert_that(postcode_details_client.invalid_postcode, is_(False))
+
+
+@pytest.mark.webtest
+def test_can_tell_if_a_postcode_is_invalid_webtest():
+    postcode_details_client = PostcodeDetailsClient(postcode='SE13BlahBlah')
+    assert_that(postcode_details_client.invalid_postcode, is_(True))
+    postcode_details_client = PostcodeDetailsClient(postcode='SE5 0NF')
+    assert_that(postcode_details_client.invalid_postcode, is_(False))
+    postcode_details_client = PostcodeDetailsClient(postcode='SH24 1AA')
+    assert_that(postcode_details_client.invalid_postcode, is_(False))
+
+
+@patch('app.postcode_details_client.requests.get')
+def test_search_failed_to_return_result_for_unknown_reason(mock_get):
+    postcode_details_client = PostcodeDetailsClient(postcode='SE13BlahBlah')
+    mock_get.return_value.ok = False
+    mock_get.return_value.json.return_value = {
+        "status": 404,
+        "error": "Invalid postcode"
+    }
+    assert_that(postcode_details_client.search_successful, is_(True))
+    postcode_details_client = PostcodeDetailsClient(postcode='SE5 0NF')
+    mock_get.return_value.ok = True
+    mock_get.return_value.json.return_value = {
+        "status": 200
+    }
+    assert_that(postcode_details_client.search_successful, is_(True))
+    postcode_details_client = PostcodeDetailsClient(postcode='SH24 1AA')
+    mock_get.return_value.ok = False
+    mock_get.return_value.json.return_value = {
+        "status": 200,
+        "error": "Postcode not found"
+    }
+    assert_that(postcode_details_client.search_successful, is_(True))
+    postcode_details_client = PostcodeDetailsClient(postcode='SH24 1AA')
+    mock_get.return_value.ok = False
+    mock_get.return_value.json.return_value = {
+        "status": 500,
+        "error": "Some other error"
+    }
+    assert_that(postcode_details_client.search_successful, is_(False))
